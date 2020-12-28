a5feafde78cc2e6d206ef19313cc4e9656110c4e
PavelLan78 <knestyapin220178@gmail.com>
2020-12-28 16:59:03 +0200

    rename fail MenuTest to MenuLeftClickTest

--- src/test/java/com/example/HW_Software_Testing_Selenium/MenuLeftClickTest.java
+++ src/test/java/com/example/HW_Software_Testing_Selenium/MenuLeftClickTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class MenuTest {
+public class MenuLeftClickTest {
     WebDriver wd;
     WebDriverWait wait;
 
